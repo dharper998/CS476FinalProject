fe350d1de3504a41748729b9322df9aaa288dc91
alison98 <nosila98@gmail.com>
2020-12-07 10:19:01 -0500

    create command line control loop

--- driver.py
+++ driver.py
@@ -20,3 +20,34 @@ with driver.session() as session:
     session.read_transaction(print_friends, "Arthur")
 
 driver.close()
+
+def main():
+    in = input("> ")
+    while(in != "exit" and in != "quit"):
+        words = input.split().lower()
+        if words[0] == "add":
+            #add users/locations
+            pass
+        elif words[0] == "delete" or words[0] == "remove":
+            pass
+        elif words[0] == "get" or words[0] == "show":
+            #find a person's risk level
+            #get each risk level
+            pass
+        elif words[1] == "contacted":
+            #add relationships
+            pass
+        elif words[1] == "went" and words[2] == "to":
+            #add relationships
+            pass
+        elif (words[1] == "tested" and words[2] == "positive") or (words[1] == "is" and words[2] == "infected"):
+            #set to infected
+            pass
+        elif words[0] == "tick":
+            pass
+        else:
+            print("input not recognized")
+        in = input("> ")
+
+
+main()
