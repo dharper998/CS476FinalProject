5d9eed88e0d965a737aaa5b7312dbcb1a324a1bc
Drew Harper <dharper998@gmail.com>
2020-12-07 22:17:50 -0500

    Updating risk queries to factor in time of interactions, updating time tick implementation, and creating individual risk level assessment function

--- driver.py
+++ driver.py
@@ -2,42 +2,46 @@ from neo4j import GraphDatabase
 
 driver = GraphDatabase.driver("bolt://127.0.0.1:7687", auth=("neo4j", "asdf"), encrypted=False)
 
-def set_infected(tx, name, days_since_test):
+#Working
+def set_infected(tx, name, status, days_since_test):
     tx.run("MATCH (p:Person {name: $name}) "
-            "SET p.infected = 'Yes' "
+            "SET p.infected = $status "
             "SET p.daysSinceTest = $daysSince",
             name=name,
+            status=status,
             daysSince=days_since_test)
 
-def update_status(tx):
+#Working
+def tick(tx):
+    tx.run("MATCH (p:Person) "
+            "WHERE NOT p.daysSinceTest = -1 "
+            "SET p.daysSinceTest = p.daysSinceTest + 1")
     tx.run("MATCH (p:Person) "
             "WHERE p.daysSinceTest >= 14 "
             "SET p.infected = 'No' "
             "SET p.daysSinceTest = -1")
-
-def tick(tx):
-    tx.run("MATCH (p:Person) "
-            "WHERE NOT p.daysSinceTest = -1 "
-            "SET p.daysSinceTest += 1")
-    tx.run("MATCH (c:CONTACTED) "
-            "SET c.daysSince += 1")
-    tx.run("MATCH (v:VISITED) "
-            "SET v.daysSince += 1")
-    tx.run("MATCH (c:CONTACTED) "
+    tx.run("MATCH ()-[c:CONTACTED]->() "
+            "SET c.daysSince = c.daysSince + 1")
+    tx.run("MATCH ()-[v:VISITED]->() "
+            "SET v.daysSince = v.daysSince + 1")
+    tx.run("MATCH ()-[c:CONTACTED]->() "
             "WHERE c.daysSince >= 14 "
             "DETACH DELETE c")
-    tx.run("MATCH (v:VISITED) "
+    tx.run("MATCH ()-[v:VISITED]->() "
             "WHERE v.daysSince >= 14 "
             "DETACH DELETE v")
 
+#Working
 def add_user(tx, name):
     tx.run("MERGE (p:Person {name: $name, infected: 'No', daysSinceTest: -1})",
             name=name)
 
+#Working
 def add_location(tx, name):
     tx.run("MERGE (l:Location {name: $name})",
             name=name)
 
+#Working
 def add_contact(tx, name1, name2, days_since):
     tx.run("MATCH (a:Person {name: $name1}) "
             "MATCH (b:Person {name: $name2}) "
@@ -45,6 +49,7 @@ def add_contact(tx, name1, name2, days_since):
             name1=name1,
             name2=name2,
             daysSince=days_since)
+
 
 def add_visited(tx, name, location, days_since):
     tx.run("MATCH (a:Person {name: $name}) "
@@ -61,22 +66,53 @@ def get_high_risk(tx):
         print(item['high']['name'])
 
 def get_medium_risk(tx):
-    medium = tx.run("MATCH (p:Person {infected:'Yes'})-[*2]-(medium) "
-            "RETURN DISTINCT medium")
+    medium = tx.run("MATCH (p:Person {infected:'Yes'})-[c]-(high)-[d]-(medium) "
+                    "WHERE d.daysSince <= c.daysSince "
+                    "RETURN DISTINCT medium")
     for item in medium.data():
         print(item['medium']['name'])
 
 def get_low_risk(tx):
-    low = tx.run("MATCH (p:Person {infected:'Yes'})-[*3]-(low) "
-            "RETURN DISTINCT low")
+    low = tx.run("MATCH (p:Person {infected:'Yes'})-[c]-(high)-[d]-(medium)-[e]-(low) "
+                    "WHERE e.daysSince <= d.daysSince <= c.daysSince "
+                    "RETURN DISTINCT low")
     for item in low.data():
         print(item['low']['name'])
+
+def get_individual_risk(tx, name):
+    status = tx.run("MATCH (p:Person {name: $name}) "
+                    "RETURN p",
+                    name=name)
+    if(status.data()[0]['p']['infected'] == 'Yes'):
+        print("Infected")
+        return
+    high = tx.run("MATCH (p:Person {infected:'Yes'})-[*1]-(high) "
+            "RETURN DISTINCT high")
+    medium = tx.run("MATCH (p:Person {infected:'Yes'})-[c]-(high)-[d]-(medium) "
+                    "WHERE d.daysSince <= c.daysSince "
+                    "RETURN DISTINCT medium")
+    low = tx.run("MATCH (p:Person {infected:'Yes'})-[c]-(high)-[d]-(medium)-[e]-(low) "
+                    "WHERE e.daysSince <= d.daysSince <= c.daysSince "
+                    "RETURN DISTINCT low")
+    for item in high.data():
+        if(item['high']['name'] == name):
+            print("High")
+            return
+    for item in medium.data():
+        if(item['medium']['name'] == name):
+            print("Medium")
+            return
+    for item in low.data():
+        if(item['low']['name'] == name):
+            print("Low")
+            return
+    print("Not at risk")
 
 def main():
     inp = input("> ")
     with driver.session() as session:
         while(inp != "exit" and inp != "quit"):
-            words = inp.lower().split()
+            words = inp.split()
             if words[0] == "tick":
                 session.write_transaction(tick)
             elif words[0] == "add":
@@ -95,7 +131,7 @@ def main():
                     elif words[1] == "high":
                         session.read_transaction(get_high_risk)
                     else:
-                        print("input not recognized")
+                        session.read_transaction(get_individual_risk, words[1])
                 else:
                     print("input not recognized")
             elif words[1] == "contacted":
@@ -103,7 +139,9 @@ def main():
             elif words[1] == "visited":
                 session.write_transaction(add_visited, words[0], " ".join(words[2:-1]), int(words[-1]))
             elif (words[1] == "tested" and words[2] == "positive") or (words[1] == "is" and words[2] == "infected"):
-                session.write_transaction(set_infected, words[0], int(words[3]))
+                session.write_transaction(set_infected, words[0], "Yes", int(words[3]))
+            elif (words[1] == "tested" and words[2] == "negative"):
+                session.write_transaction(set_infected, words[0], "No", int(words[3]))
             else:
                 print("input not recognized")
             inp = input("> ")
